ef0888ecfbf7148ac0a3e3baf9c0217de6a26c62
Robert Weaver <r.weaver17751@gmail.com>
2022-10-08 09:50:11 -0400

    upo

--- Login/LoginProject/views.py
+++ Login/LoginProject/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+class HomeView(ListView):
+      model = Post
+      template_name = 'home.html'
+      
+      def get_context_data(self, *args, **kwargs):
+        cat_menu = User.objects.all()
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["cat_menu"] = cat_menu
+        return context
